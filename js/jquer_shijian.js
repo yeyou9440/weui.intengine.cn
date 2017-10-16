f6f7f13f5f5086aa796cb0970f55b08ed2d1d5d0
yeyou9440 <yeyou9440@163.com>
2017-10-16 17:42:52 +0800

    333

--- js/jquer_shijian.js
+++ js/jquer_shijian.js
@@ -78,6 +78,15 @@
           var seperator1 = "-";
           var month = mydate.getMonth() + 1;
           if (!options.mmArr) this.mmArr[0] = mydate.getMonth() + 1;
+
+                var GetDateStr = function(AddDayCount) {
+                  var dd = new Date();
+                  dd.setDate(dd.getDate() + AddDayCount); //获取AddDayCount天后的日期
+                  var y = dd.getFullYear();
+                  var m = (dd.getMonth() + 1) < 10 ? "0" + (dd.getMonth() + 1) : (dd.getMonth() + 1); //获取当前月份的日期，不足10补0
+                  var d = dd.getDate() < 10 ? "0" + dd.getDate() : dd.getDate(); //获取当前几号，不足10补0
+                  return y + "-" + m + "-" + d;
+                };
           for (var i = 0; i < 7; i++) {
             if (i < 7 && !options.ddArr) {
               this.ddArr[i] = GetDateStr(i);
@@ -744,14 +753,6 @@
         return Math.atan2(dy, dx) * 180 / Math.PI;
       }
 
-      var GetDateStr =function(AddDayCount) {
-        var dd = new Date();
-        dd.setDate(dd.getDate() + AddDayCount); //获取AddDayCount天后的日期
-        var y = dd.getFullYear();
-        var m = (dd.getMonth() + 1) < 10 ? "0" + (dd.getMonth() + 1) : (dd.getMonth() + 1); //获取当前月份的日期，不足10补0
-        var d = dd.getDate() < 10 ? "0" + dd.getDate() : dd.getDate(); //获取当前几号，不足10补0
-        return y + "-" + m + "-" + d;
-      };
       var GetSlideDirection = function(startX, startY, endX, endY) { //判读手指滑动方向
         var dy = startY - endY;
         var dx = endX - startX;
