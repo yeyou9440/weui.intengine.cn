8f56f0adc188859783912b3be0ac5f1016b09280
yeyou9440 <yeyou9440@163.com>
2017-10-16 17:32:14 +0800

    33

--- js/jquer_shijian.js
+++ js/jquer_shijian.js
@@ -75,7 +75,7 @@
           var m = (dd.getMonth() + 1) < 10 ? "0" + (dd.getMonth() + 1) : (dd.getMonth() + 1); //获取当前月份的日期，不足10补0
           var d = dd.getDate() < 10 ? "0" + dd.getDate() : dd.getDate(); //获取当前几号，不足10补0
           return y + "-" + m + "-" + d;
-        }
+        },
         getArr: function() {
           //按时间生成分钟，小时，月天数，月份
 
